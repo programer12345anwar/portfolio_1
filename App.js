f0477f8c245476d0bfaebe3c225a92f4cc682ee7
Md Anwar Alam <mdanwar40212@gmail.com>
2024-02-29 01:08:08 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hambergor-icon");
+    menu.classList("open");
+    icon.classList("open");
+}
